22e640abfeb77d1513537147251d0e18efd9af73
Sophie Gershon <sophgersh@gmail.com>
2015-05-06 18:47:28 -0400

    can create a route object with origin, destination, mode --> calculates time + distance; different colors for pins

--- static/js/maps.js
+++ static/js/maps.js
@@ -1,10 +1,18 @@
-
 var map;
-var geocoder = new google.maps.Geocoder();
+var geocoder = new google.maps.Geocoder(); 
+// https://developers.google.com/maps/documentation/geocoding/
+
 var defaultMarker = {
     animation: google.maps.Animation.DROP,
     draggable: true
 }
+
+/*
+marker = new google.maps.Marker({
+    icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'
+    other colors: red, purple, yellow, green
+});
+*/
 
 var initialize = function(){
     var mapOptions = {
@@ -48,10 +56,11 @@ var markCurrent = function(){
 	    initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
 	    map.setCenter(initialLocation);
 	    var marker = new google.maps.Marker({
-		    map: map,
-		    position: initialLocation,
-		    animation: defaultMarker.animation,
-		    draggable: defaultMarker.draggable
+		map: map,
+		position: initialLocation,
+		animation: defaultMarker.animation,
+		draggable: defaultMarker.draggable,
+		icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'
 	    });
 	    console.log(marker.getPosition());
 	    addPlace(marker.getPosition());
@@ -74,19 +83,12 @@ var addPlace = function(latlong){
     geocoder.geocode({'location': latlong}, function(results,status){
 	if (status == google.maps.GeocoderStatus.OK) {
 	    var address = results[0].formatted_address;
-
 	    var li = document.createElement("li");
 	    var node = document.createTextNode(address);
 	    li.appendChild(node);
-
 	    var element = document.getElementById("list");
 	    element.appendChild(li);
-
-	    //var list = document.getElementById("list");
-	    /*var li = document.createElement("li");
-	    var node = document.createTextNode(address);
-	    list.appendChild(li.appendChild(node));*/
-	    //list.append("<li>"+address+"<li>");
+	   
 	} else {
 	    console.log("no work");
 	}
@@ -94,17 +96,74 @@ var addPlace = function(latlong){
 }
 
 
-//function
-/*function loadScript(){
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp' +
-      '&signed_in=true&callback=initialize';
-    document.body.appendChild(script);
+
+var service = new google.maps.DistanceMatrixService();
+
+
+
+var route = function(origin, destination, mode){
+    return {
+	origin : origin,
+	destination : destination,
+	mode : google.maps.TravelMode[mode],
+	time : "",
+	distance : "",
+	calculate : function(){
+	    service.getDistanceMatrix({
+		origins: [this.origin],
+		destinations: [this.destination],
+		travelMode: this.mode,
+		unitSystem: google.maps.UnitSystem.IMPERIAL,
+	    }, callback);
+	    // after successful call to API, calls function callback
+	    function callback(response, status) {
+		console.log("in callback");
+		var element = response.rows[0].elements[0];
+		this.distance = element.distance.text;
+		console.log(this.distance);
+		this.time = element.duration.text;
+		console.log("got distance and time");
+	    } 
+	},
+	getDistance : function(){ return distance; },
+	getTime : function(){ return time; }
+	
+    }
 }
 
-window.onload = loadScript;*/
+var r = route("275 W. 96th St. NY","345 Chambers St. NY","BICYCLING");
+r.calculate();
+
+
+var getDistance = function(origin, destination, mode){
+    //origin and destination can be any type of maps object thing
+    //mode can be: WALKING, DRIVING (default), BICYCLING
+    //google.maps.TravelMode.WALKING 
+    //var distance;
+    //var time;
+    service.getDistanceMatrix({
+	origins: [origin],
+	destinations: [destination],
+	travelMode: google.maps.TravelMode.DRIVING,
+	//transitOptions: TransitOptions,
+	unitSystem: google.maps.UnitSystem.IMPERIAL,
+    }, callback);
+    // after successful call to API
+    function callback(response, status) {
+	var element = response.rows[0].elements[0];
+        route.distance = element.distance.text;
+        route.time = element.duration.text;
+
+	//console.log("time: "+time);
+	//console.log("distance: "+distance);
+	
+	//return {"time": time, "distance":distance};
+    } 
+    
+}
+
 
 google.maps.event.addDomListener(window, 'load', initialize);
 document.getElementById("mark-address").addEventListener("click",placeMarker);
 document.getElementById("current").addEventListener("click",markCurrent);
+
